94356fecb28b4194cf31ac542929edec1f9369cb
kimyeonju <duswn4338@gmail.com>
2021-04-20 15:46:45 +0900

    public폴더- data,images추가 / index.js- reset,common sass import완료

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Routes from './Routes';
+import './styles/reset.scss';
+import './styles/commons.scss';
 
 ReactDOM.render(<Routes />, document.getElementById('root'));
