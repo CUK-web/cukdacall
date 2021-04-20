d45c421540f8dc19ab1a77c31f7c1474869a0d1a
kimyeonju <duswn4338@gmail.com>
2021-04-20 15:36:07 +0900

    footer ui 완성

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -5,19 +5,21 @@ import ClassEval from './pages/classeval/ClassEval';
 import InSchool from './pages/inschool/InSchool';
 import Main from './pages/main/Main';
 import OutSchool from './pages/outschool/OutSchool';
+import Footer from './components/footer/Footer';
 
 class Routes extends Component {
   render() {
     return (
       <Router>
-        <Link to="/main">Main</Link>
+        {/* <Link to="/main">Main</Link>
         <Link to="/insideschool">Inside School</Link>
         <Link to="/outsideschool">Outside School</Link>
         <Link to="/academiccalender">Academic Calender</Link>
-        <Link to="/classevaluation">Class Evaluation</Link>
+        <Link to="/classevaluation">Class Evaluation</Link> */}
         <Switch>
           <Route exact path="/main" component={Main} />
           <Route exact path="/insideschool" component={InSchool} />
+          <Route exact path="/footer" component={Footer} />
           <Route exact path="/outsideschool" component={OutSchool} />
           <Route exact path="/academiccalender" component={AcademCal} />
           <Route exact path="/classevaluation" component={ClassEval} />
