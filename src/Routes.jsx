d6cb88871e457572e77de664365691baf093b442
kimyeonju <duswn4338@gmail.com>
2021-04-29 12:12:59 +0900

    conflict: conflict해결

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -13,7 +13,6 @@ class Routes extends Component {
   render() {
     let HideNav = window.location.pathname === '/' ? null : <Nav />;
     return (
-<<<<<<< HEAD
       <Router>
         <Switch>
           <Route exact path="/" component={Main} />
@@ -27,30 +26,6 @@ class Routes extends Component {
         </Switch>
         <Footer />
       </Router>
-=======
-      // <Nav>
-      <>
-        <Router>
-          {/* <Link to="/main">Main</Link>
-          <Link to="/nav">Nav</Link>
-          <Link to="/footer">footer</Link>
-          <Link to="/insideschool">Inside School</Link>
-          <Link to="/outsideschool">Outside School</Link>
-          <Link to="/academiccalender">Academic Calender</Link>
-          <Link to="/classevaluation">Class Evaluation</Link> */}
-          {HideNav}
-          {/* <Nav /> */}
-          <Switch>
-            <Route exact path="/" component={Main} />
-            <Route exact path="/insideschool" component={InSchool} />
-            <Route exact path="/outsideschool" component={OutSchool} />
-            <Route exact path="/academiccalender" component={AcademCal} />
-            <Route exact path="/classevaluation" component={ClassEval} />
-          </Switch>
-          <Footer />
-        </Router>
-      </>
->>>>>>> 95d71c1e1220aa186c08fccac7aa2c512a4e2794
     );
   }
 }
