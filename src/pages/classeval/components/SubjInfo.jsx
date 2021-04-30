054b7e44bf24f6b5f11b97e162038e57848c59d8
MGanom <chewy111@naver.com>
2021-04-30 16:28:42 +0900

    디자인: classeval scss 작업 중

--- src/pages/classeval/components/SubjInfo.jsx
+++ src/pages/classeval/components/SubjInfo.jsx
@@ -19,29 +19,18 @@ class SubjInfo extends Component {
 
     return (
       <div className="subjInfo">
-        과목이름: {name}
-        <br />
-        교수이름: {prof}
-        <br />
-        과목번호: {subjnum}
-        <br />
-        수강학기: {semester}
-        <br />
-        교재정보: {book}
-        <br />
-        과제: {assign}
-        <br />
-        팀플: {teamplay}
-        <br />
-        학점비율: {grade}
-        <br />
-        출결: {attendance}
-        <br />
-        시험횟수: {test}
-        <br />
-        총점: {score}
-        <br />
-        총평: {review}
+        <span>과목이름:</span> <>{name}</>
+        <div>교수이름: {prof}</div>
+        <div>과목번호: {subjnum}</div>
+        <div>수강학기: {semester}</div>
+        <div>교재정보: {book}</div>
+        <div>과제: {assign}</div>
+        <div>팀플: {teamplay}</div>
+        <div>학점비율: {grade}</div>
+        <div>출결: {attendance}</div>
+        <div>시험횟수: {test}</div>
+        <div>총점: {score}</div>
+        <div>총평: {review}</div>
       </div>
     );
   }
