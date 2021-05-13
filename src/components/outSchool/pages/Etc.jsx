bfc38c02027c116ef4dc61e9b4abbe9870949c2c
MGanom <chewy111@naver.com>
2021-05-13 21:36:53 +0900

    update & design: outschool 기능 완성, classeval과 inschool 코드 수정 및 디자인

--- src/components/outSchool/pages/Etc.jsx
+++ src/components/outSchool/pages/Etc.jsx
@@ -4,10 +4,46 @@ import PlacePhoto from '../tabMenu/PlacePhoto';
 export default class Etc extends Component {
   state = {
     data: [
-      { title: '하이', imgData: '', info: '설명1' },
-      { title: '하이', imgData: '', info: '설명2' },
-      { title: '하이', imgData: '', info: '설명3' },
-      { title: '하이', imgData: '', info: '설명4' },
+      {
+        title: '경인문고',
+        imgData: 'kyungin.png',
+        info: '매일 10:00-22:00 / 설날, 추석날 휴무',
+      },
+      {
+        title: '다큐 카피',
+        imgData: 'docucopy.png',
+        info: '평일 09:00-19:40 / 주말, 공휴일 휴무',
+      },
+      {
+        title: '교내 복사실',
+        imgData: 'gyonae.png',
+        info: '평일 08:00-20:00 / 토 10:00-16:00 / 일, 공휴일 휴무',
+      },
+      {
+        title: '용서점',
+        imgData: 'yongseo.png',
+        info: '12:00-20:00 / 월, 일 휴무',
+      },
+      {
+        title: '프린트 카페',
+        imgData: 'printcafe.png',
+        info: '매일 00:00-24:00',
+      },
+      {
+        title: '알파 문구',
+        imgData: 'alpha.png',
+        info: '평일 08:30-20:00 / 토요일 09:00-19:00 / 일요일 휴무',
+      },
+      {
+        title: '그린팜 마트',
+        imgData: 'greenfarm.png',
+        info: '매일 00:00-24:00',
+      },
+      {
+        title: '홈플러스 익스프레스',
+        imgData: 'homeplus.png',
+        info: '매일 10:00-23:00 / 둘째, 넷째 일요일 휴무',
+      },
     ],
   };
 
